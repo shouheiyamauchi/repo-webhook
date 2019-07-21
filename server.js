2064fa4150f8bc8054b7087360b2659b0f0ee520
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2019-07-21 22:49:30 +1000

    repo webhook app

---
+++ server.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser')
+const crypto = require('crypto')
+const exec = require('child_process').exec
+const dotenv = require('dotenv')
+const express = require('express')
+
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const validateRequest = (body, headers) => {
+  const payload = JSON.stringify(body)
+  const hmac = crypto.createHmac('sha1', process.env.SECRET)
+  const digest = 'sha1=' + hmac.update(payload).digest('hex')
+
+  return digest === headers['x-hub-signature']
+}
+
+app.post('/', (req, res) => {
+  if (validateRequest(req.body, req.headers) && req.body.ref === 'refs/heads/master') {
+    exec('cd ~/apps/sm-api && git pull origin master')
+  }
+
+  res.send()
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log(`repo-webhook running on port ${port}`)
+})
